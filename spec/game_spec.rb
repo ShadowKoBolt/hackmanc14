079cb3db40a5f4472af92788a0c3a985b595b364
Luke Roberts <email@luke-roberts.co.uk>
2014-10-25 13:25:54 +0100

    Base -> Tower

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,6 +1,8 @@
 require './app/game.rb'
 
 describe Game do
-  subject { Game.new('identifiable_string', 'another_identifiable_string') }
-  specify { subject.to_json.should be == "{\"bases\":[\"identifiable_string\",\"another_identifiable_string\"],\"players\":[],\"enemies\":[]}" }
+  let(:tower_1) { double('Tower 1', {as_json:'identifiable_string'}) }
+  let(:tower_2) { double('Tower 1', {as_json:'another_identifiable_string'}) }
+  subject { Game.new(tower_1, tower_2) }
+  specify { subject.to_json.should be == "{\"towers\":[\"identifiable_string\",\"another_identifiable_string\"],\"players\":[],\"enemies\":[]}" }
 end
