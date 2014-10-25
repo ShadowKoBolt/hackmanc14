90861d3e5142fdf9f42e546c82d568f68a6c8fc0
Nick Bolt <bolt.shadow@gmail.com>
2014-10-25 17:22:08 +0100

    beacons workingish

--- android/DOTB/app/src/main/java/net/llamadigital/dotb/WebViewActivity.java
+++ android/DOTB/app/src/main/java/net/llamadigital/dotb/WebViewActivity.java
@@ -4,8 +4,6 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -14,17 +12,21 @@ import com.estimote.sdk.Beacon;
 import com.estimote.sdk.BeaconManager;
 import com.estimote.sdk.Region;
 
+import net.llamadigital.dotb.beacons.Processor;
+
 import java.util.List;
 
 
 public class WebViewActivity extends Activity {
 
+    private static final boolean RANGING = true;
     private static final String LOG_TAG = WebViewActivity.class.getName();
 
-    private static final String ESTIMOTE_PROXIMITY_UUID = "48AAD310-D5D2-11E3-9C1A-0800200C9A66";
+    private static final String ESTIMOTE_PROXIMITY_UUID = "B9407F30-F5F8-466E-AFF9-25556B57FE6D";
     private static final Region ALL_ESTIMOTE_BEACONS = new Region("regionId", ESTIMOTE_PROXIMITY_UUID, null, null);
 
-    private BeaconManager beaconManager = new BeaconManager(this);
+    private BeaconManager mBeaconManager = new BeaconManager(this);
+    private Processor mProcessor = new Processor();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +34,7 @@ public class WebViewActivity extends Activity {
         setContentView(R.layout.activity_web_view);
 
         // Webview setup
-        WebView webView = (WebView) findViewById(R.id.activity_web_view_web_view);
+        final WebView webView = (WebView) findViewById(R.id.activity_web_view_web_view);
         webView.setWebViewClient(new WebViewClient() {
             public boolean shouldOverrideUrlLoading(WebView view, String url){
                 view.loadUrl(url);
@@ -44,40 +46,51 @@ public class WebViewActivity extends Activity {
         webView.loadUrl(getString(R.string.base_url));
 
         // Ranging setup
-        beaconManager.setRangingListener(new BeaconManager.RangingListener() {
-            @Override public void onBeaconsDiscovered(Region region, List<Beacon> beacons) {
-                Log.d(LOG_TAG, "Ranged beacons: " + beacons);
-            }
-        });
+        if (RANGING == true) {
+            mBeaconManager.setRangingListener(new BeaconManager.RangingListener() {
+                @Override
+                public void onBeaconsDiscovered(Region region, List<Beacon> beacons) {
+                    Integer bestBeaconMajor = mProcessor.getBestBeaconMajorInList(beacons);
+                    webView.loadUrl("javascript:test('Beacon " + bestBeaconMajor + "')");
+                }
+            });
+        }
     }
 
     @Override
     protected void onStart() {
-        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
-            @Override public void onServiceReady() {
-                try {
-                    beaconManager.startRanging(ALL_ESTIMOTE_BEACONS);
-                } catch (RemoteException e) {
-                    Log.e(LOG_TAG, "Cannot start ranging", e);
+        if (RANGING == true) {
+            mBeaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+                @Override
+                public void onServiceReady() {
+                    try {
+                        mBeaconManager.startRanging(ALL_ESTIMOTE_BEACONS);
+                    } catch (RemoteException e) {
+                        Log.e(LOG_TAG, "Cannot start ranging", e);
+                    }
                 }
-            }
-        });
+            });
+        }
         super.onStart();
     }
 
     @Override
     protected void onStop() {
-        try {
-            beaconManager.stopRanging(ALL_ESTIMOTE_BEACONS);
-        } catch (RemoteException e) {
-            Log.e(LOG_TAG, "Cannot stop but it does not matter now", e);
+        if (RANGING == true) {
+            try {
+                mBeaconManager.stopRanging(ALL_ESTIMOTE_BEACONS);
+            } catch (RemoteException e) {
+                Log.e(LOG_TAG, "Cannot stop but it does not matter now", e);
+            }
         }
         super.onStop();
     }
 
     @Override
     protected void onDestroy() {
-        beaconManager.disconnect();
+        if (RANGING == true) {
+            mBeaconManager.disconnect();
+        }
         super.onDestroy();
     }
 }
