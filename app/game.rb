74a2a87f4fcd404a57457c392735696982525b87
Luke Roberts <email@luke-roberts.co.uk>
2014-10-25 20:52:30 +0100

    some more shiz, btw where did that beacon nick took go??? nobody knows....

--- app/game.rb
+++ app/game.rb
@@ -42,8 +42,8 @@ class Game
   end
 
   def decrement_towers!
-    new_health = (@towers.inject(0) { |res, t| res + t.enemies })
-    @health = [new_health, 0].max
+    damage = (@towers.inject(0) { |res, t| res + t.enemies })
+    @health = [(@health - damage), 0].max
   end
 
   def new_player_from_connection(connection)
