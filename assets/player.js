8a56ecb0cb58b1b50dde09724b25f4e39d441043
vagrant <vagrant@hackman14>
2014-10-25 13:57:07 +0100

    add websocket connection

--- assets/player.js
+++ assets/player.js
@@ -1,1 +1,16 @@
-alert("test");
+$(function() {
+  var DOTB = DOTB || {};
+  DOTB.Game = {
+    setup: function(gameScreen) {
+      this.connection = new WebSocket('ws://192.168.69.69:8080');
+      this.connection.onmessage = function (e) {
+        updateGame(e.data);
+      }
+    },
+    updateGame: function(newGame) {
+      console.log(newGame);
+    }
+  }
+
+  DOTB.Game.setup($('body'));
+});
