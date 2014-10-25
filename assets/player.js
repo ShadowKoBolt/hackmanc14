583ccd926c38395fb5f23c59499fc9eb394ce1e3
Daniel King <dan@llamadigital.net>
2014-10-25 16:57:47 +0100

    add location and enemies

--- assets/player.js
+++ assets/player.js
@@ -4,6 +4,8 @@ $(function() {
     // Setup
     var self = this;
     this.view = view;
+    this.enemies = 0;
+    this.currentLocation = 0;
     this.connection = new WebSocket('wss://hackman.llamadigital.net:8080');
     this.connection.onmessage = function (e) {
       var parsedData = JSON.parse(e.data);
@@ -17,12 +19,21 @@ $(function() {
     function updateGame(newGame) {
       console.log(newGame);
       view.find('#base-status h2').text(newGame.health);
+      self.enemies = newGame.towers[self.currentLocation].enemies;
     }
     function attack() {
-      self.connection.send('attack');
+      var newAttack = { "attack": 1 }
+      self.connection.send(JSON.stringify(newAttack));
     }
 
     this.updateLocation = function(location) {
+      self.currentLocation = location;
+      if (location == 0) {
+        view.find('#current-location h2').text('Base');
+      } else {
+        view.find('#current-location h2').text('Tower: '+ location);
+        view.find('#enemies').text(self.enemies);
+      }
       var newLocation = { "location": location }
       self.connection.send(JSON.stringify(newLocation));
     }
