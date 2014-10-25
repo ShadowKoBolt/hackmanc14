cd6e42f87276bcf653f7cac95d209a4bfe50ffb8
vagrant <vagrant@hackman14>
2014-10-25 15:09:57 +0100

    add attack function

--- assets/player.js
+++ assets/player.js
@@ -1,16 +1,27 @@
 $(function() {
-  var DOTB = DOTB || {};
-  DOTB.Game = {
-    setup: function(gameScreen) {
-      this.connection = new WebSocket('ws://192.168.69.69:8080');
-      this.connection.onmessage = function (e) {
-        updateGame(e.data);
-      }
-    },
-    updateGame: function(newGame) {
+  function Game(view) {
+
+    // Setup
+    this.view = view;
+    this.connection = new WebSocket('ws://hackman.llamadigital.net:8080');
+    this.connection.onmessage = function (e) {
+      var parsedData = JSON.parse(e.data);
+      updateGame(parsedData);
+    }
+    view.find('#attack').click(function() {
+      attack();
+    });
+
+
+    function updateGame(newGame) {
       console.log(newGame);
+      view.find('#base-status h2').text(newGame.health);
+    }
+    function attack() {
+      connection.send('attack');
     }
   }
 
-  DOTB.Game.setup($('body'));
+  var game = new Game($('body'));
+  window.test = function(string) { console.log(string); }
 });
